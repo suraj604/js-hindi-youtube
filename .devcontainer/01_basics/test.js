bbb0e81f362bb38b0bf142eb366fa599f2ab29c0
suraj604 <112873985+suraj604@users.noreply.github.com>
2025-03-18 05:34:59 +0000

    done sAve it

--- .devcontainer/01_basics/test.js
+++
@@ -1,6 +0,0 @@
-const accountId = 14453
-let account email = mahapatro45@gmail.com
-var account password = "aryasha@1432"
-accoutcity = "berhampur"
-
-console console.table([account email, accountId, accountcity,account paasword]);
---
+++ 01_basics/test.js
@@ -0,0 +1,7 @@
+const accountid =144536
+let accountpassword = "123456"
+var accountEmail = "mahapatro45@gmail.com"
+let accountcity ="berhampur"
+// var is not preferable to use now a days.
+ console.table([accountid,accountpassword,accountEmail,accountcity]);
+ 
